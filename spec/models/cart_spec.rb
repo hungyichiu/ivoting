c06f4b5c583ce570770057572b0791ec8f24bc57
Chris Chiu <hungyichiu@gmail.com>
2019-09-13 14:46:01 +0800

    test:商品可以放到購物車裡，也可以再拿出來  finished

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe Cart, type: :model do
       end
       it "如果加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
         cart= Cart.new
-        # p1 = Product.create(title: 'p1', price: 100)
-        # p2 = Product.create(title: 'p2', price: 50)
 
         5.times {cart.add_item(1)}
         3.times {cart.add_item(2)}
@@ -19,7 +17,20 @@ RSpec.describe Cart, type: :model do
         expect(cart.items.first.quantity).to be 5
         expect(cart.items.last.quantity).to be 3
       end
-      it "商品可以放到購物車裡，也可以再拿出來"
+      it "商品可以放到購物車裡，也可以再拿出來" do
+        cart = Cart.new
+        p1 = Product.create(name: 'OnePunch')
+        p2 = Product.create(name: 'Hunter')
+
+        5.times {cart.add_item(p1.id)}
+        3.times {cart.add_item(p2.id)}
+
+        expect(cart.items.first.product_id).to be p1.id
+        expect(cart.items.last.product_id).to be p2.id
+        expect(cart.items.first.product).to be_a Product
+
+
+      end
       it "每個 Cart Item 都可以計算它自己的金額（小計）"
       it "可以計算整台購物車的總消費金額"
       it "特別活動可能可搭配折扣（例如聖誕節的時候全面打 9 折，或是滿額滿千送百）"
